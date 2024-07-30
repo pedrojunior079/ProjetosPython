3b60fcdeae4fe51e3296eb8ac691581b6b1aea23
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-07-30 17:30:13 -0300

    Escrevendo sua primeira aplicação Django, parte 2

--- TutorialDjango/pesquisas/admin.py
+++ TutorialDjango/pesquisas/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Pergunta
 
-# Register your models here.
+admin.site.register(Pergunta)
--- TutorialDjango/pesquisas/models.py
+++ TutorialDjango/pesquisas/models.py
@@ -1,3 +1,23 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Pergunta(models.Model):
+    pergunta_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("data de publicação")
+
+    def __str__(self):
+        return self.pergunta_text
+    
+    def was_published_recentl(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Escolha(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    escolha_text = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.escolha_text
