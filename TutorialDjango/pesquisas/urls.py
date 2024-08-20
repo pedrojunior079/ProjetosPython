d649633f9f5c1289d33f4f094ba642824edb4758
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-08-20 16:10:31 -0300

    upgrade gitignore

--- TutorialDjango/pesquisas/urls.py
+++ TutorialDjango/pesquisas/urls.py
@@ -2,7 +2,17 @@ from django.urls import path
 
 from . import views
 
+app_name = "pesquisas"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /pesquisas/5/
+    path("<int:pergunta_id>/", views.detail, name="detail"),
+    # ex: /pesquisas/5/resultados/
+    path("<int:pergunta_id>/resultados/", views.resultados, name="resultados"),
+    # ex: /pesquisas/5/voto/
+    path("<int:pergunta_id>/voto/", views.voto, name="voto"),
+    # added the word 'detalhes'
+    path("detalhes/<int:pergunta_id>/", views.detail, name="detail"),
 ]
 
--- TutorialDjango/pesquisas/views.py
+++ TutorialDjango/pesquisas/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
-
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import Pergunta
 
 def index(request):
-    return HttpResponse("Olá, mundo. Você está no índice de pesquisas.")
+    latest_pergunta_list= Pergunta.objects.order_by("-pub_date")[:5]
+    context = {"latest_pergunta_list": latest_pergunta_list}
+    return render(request, "pesquisas/index.html",context)
+
+def detail(request, pergunta_id):
+    pergunta = get_object_or_404(pergunta, pk=pergunta_id)    
+    return render(request, "pesquisas/detail.html", {"pergunta": pergunta})
+
+def resultados(request, pergunta_id):
+    response = "Voce esta olhando os resultados da pergunta %s."
+    return HttpResponse(response % pergunta_id)
+
+def voto(request, pergunta_id):
+    return HttpResponse("Voce está votando na questão %s." % pergunta_id)
+
